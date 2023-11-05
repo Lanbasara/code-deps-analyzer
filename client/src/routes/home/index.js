6c17ee11504c5a0889649393afce511b6ce8a66a
Lanbasara <hg1483@nyu.edu>
2023-11-05 14:01:10 +0800

    2023-11-05 init client preact

---
+++ client/src/routes/home/index.js
@@ -0,0 +1,12 @@
+import { h } from 'preact';
+import style from './style.css';
+
+const Home = () => {
+  return (
+    <div class={style.home}>
+      <h1>Get Started Building PWAs with Preact-CLI</h1>
+    </div>
+  );
+};
+
+export default Home;
